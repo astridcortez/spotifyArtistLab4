a79b6d144447cfdec18dca1832ff110834e20686
Esther Cortez <70614557+astridcortez@users.noreply.github.com>
2020-10-25 23:54:09 -0400

    Update VIPClients.java

--- VIPClients.java
+++ VIPClients.java
@@ -81,13 +81,13 @@ public class VIPClients {
                 if(array[j][1].compareToIgnoreCase(array[j+1][1])>0){
                     switched = true;
                 
-                    temp[0][0] = array[j][0];
+                    temp[0][1] = array[j][1];
                     temp[0][1] = array[j][1];
                     
-                    array[j][0] = array [j+1][0];
+                    array[j][1] = array [j+1][1];
                     array[j][1] = array [j+1][1];
                     
-                    array[j+1][0] = temp[0][0];
+                    array[j+1][1] = temp[0][1];
                     array[j+1][1] = temp[0][1];
                 }
             }
